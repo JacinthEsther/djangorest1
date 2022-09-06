b4c9469f86a816b241a8707331b0eb7233e6596d
Agboniro Esther Jacinta <agbonirojacinta@gmail.com>
2022-09-06 17:05:53 +0100

    changed files

--- my_first_rest_project/settings.py
+++ my_first_rest_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6odd+e$wl+a)l%*(9b*g)b_pyq!7c8!dykos30r+*_7h)%00+h'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
